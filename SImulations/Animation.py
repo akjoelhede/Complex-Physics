9842be60de4e910cb9c56ccbe5aa283587312aa7
akjoelhede <terpanders@gmail.com>
2022-09-30 19:18:54 +0200

    a

--- SImulations/Animation.py
+++ SImulations/Animation.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib import animation
+import matplotlib.animation as animation
+from tqdm import tqdm
 
 
 #input: L = initial lattice
@@ -11,7 +12,7 @@ def Soc(L):#, N, T):
 	lattice[1:N+1, 1:N+1] = L
 
 	#for t in range(0,T):
-	if np.any(4 < lattice[1:N+1, 1:N+1]):
+	if np.all(lattice < 4 ):
 		r1 = np.random.randint(1,N)
 		r2 = np.random.randint(1,N)
 		lattice[r1,r2]+=1
@@ -22,23 +23,32 @@ def Soc(L):#, N, T):
 		for i in range(1, len(lattice)-1):
 			for j in range(1, len(lattice)-1):
 				if lattice[i,j]>=4:
-					add[i,j] -= -4
+					add[i,j] -= 4
 					add[i+1,j] += 1
 					add[i,j+1] += 1
 					add[i-1,j] += 1
 					add[i,j-1] += 1
-		lattice = lattice+add
+		lattice = lattice + add
 	return lattice[1:N+1, 1:N+1]
 
 a100 = []
-a=np.random.randint(4,size = (20,20))
-for i in range(1000):
+a=np.random.randint(6,size = (100,100))
+for i in tqdm(range(1000)):
 	a100.append(a)
 	a = Soc(a)
 
 print(a100[1].sum(), a100[999].sum())
 
-img = plt.imshow(a100[999])
-plt.colorbar(img)
+fig = plt.figure(figsize = (8,8))
+im = plt.imshow(a100[0])
+plt.colorbar()
+def animate_func(i):
+	im.set_array(a100[i])
+	return [im]
+
+anim = animation.FuncAnimation(fig, animate_func, frames = 1000, interval = 20)
 plt.show()
+#img = plt.imshow(a100[999])
+#plt.colorbar(img)
+#plt.show()
 
