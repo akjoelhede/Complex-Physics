fd97d5c13c188c7fcddb4f463aedb45726a55676
akjoelhede <63365928+akjoelhede@users.noreply.github.com>
2022-10-06 15:24:49 +0200

    a

--- Midterm_2/Self_organized_criticality.py
+++ Midterm_2/Self_organized_criticality.py
@@ -59,23 +59,23 @@ def Soc(L, counts = 0):#, N, T):
 	#avalanche.append(counts)
 
 
-	return lattice[1:N+1, 1:N+1], counts 
+	return counts, lattice[1:N+1, 1:N+1]
 
 
-a1=np.random.randint(2,size = (200,200))
-#a2=np.random.randint(2,size = (50,50))
-#a3=np.random.randint(2,size = (100,100))
-#a4=np.random.randint(2,size = (200,200))
+a1=np.random.randint(2,size = (25,25))
+a2=np.random.randint(2,size = (50,50))
+a3=np.random.randint(2,size = (100,100))
+a4=np.random.randint(2,size = (200,200))
 
 #@njit
 def sandpile(initial):
-	a100 = []
+	#a100 = []
 	b100 = []
 	a=initial
 	c = 0
 	for i in tqdm(range(400000)):
-		a100.append(a)
-		a,c_new = Soc(a,c)
+		#a100.append(a)
+		c_new,a = Soc(a,c)
 		if c_new == 0:
 			if c != 0:
 				b100.append(c)
@@ -83,9 +83,9 @@ def sandpile(initial):
 
 		else:
 			c = c_new
-
 	return b100
 
+#print(sandpile(a1))
 #anim_50 = sandpile(a2)[0]
 #print(a100[1].sum(), a100[999].sum())
 
@@ -101,9 +101,9 @@ def sandpile(initial):
 #plt.close('All')
 
 sp_25 = sandpile(a1)
-#sp_50 = sandpile(a2)
-#sp_100 = sandpile(a3)
-#sp_200 = sandpile(a4)
+sp_50 = sandpile(a2)
+sp_100 = sandpile(a3)
+sp_200 = sandpile(a4)
 
 plt.hist(sp_25, density = True)
 plt.show()
@@ -112,16 +112,19 @@ plt.close('All')
 
 
 y0, x0 = np.histogram(sp_25, bins = np.logspace(start = np.log(1), stop = np.log(np.max(sp_25))),  density = True)
-#y1, x1 = np.histogram(sp_50, bins = np.logspace(start = np.log(1), stop = np.log(np.max(sp_50))),  density = True)
-#y2, x2 = np.histogram(sp_100, bins = np.logspace(start = np.log(1), stop = np.log(np.max(sp_100))),  density = True)
-#y3, x3 = np.histogram(sp_200, bins = np.logspace(start = np.log(1), stop = np.log(np.max(sp_200))),  density = True)
+y1, x1 = np.histogram(sp_50, bins = np.logspace(start = np.log(1), stop = np.log(np.max(sp_50))),  density = True)
+y2, x2 = np.histogram(sp_100, bins = np.logspace(start = np.log(1), stop = np.log(np.max(sp_100))),  density = True)
+y3, x3 = np.histogram(sp_200, bins = np.logspace(start = np.log(1), stop = np.log(np.max(sp_200))),  density = True)
 
 plt.plot(x0[1:50], y0, 'o', label = 'N = 25')
-#plt.plot(x1[1:50], y1, 'o', label = 'N = 50')
-#plt.plot(x2[1:50], y2, 'o', label = 'N = 100')
-#plt.plot(x3[1:50], y3, 'o', label = 'N = 100')
+plt.plot(x1[1:50], y1, 'o', label = 'N = 50')
+plt.plot(x2[1:50], y2, 'o', label = 'N = 100')
+plt.plot(x3[1:50], y3, 'o', label = 'N = 200')
 plt.xscale('log')
 plt.yscale('log')
+plt.title('Powerlaws for diff. system sizes')
+plt.xlabel('log(s)')
+plt.ylabel('log(P(s))')
 plt.xlim(0, 10**6)
 plt.legend()
 plt.show()
@@ -144,5 +147,3 @@ plt.show()
 plt.close('All')
 
 
-
-# %%
